383b1f28dd7dc627f353c5ccf88b90ce119c15c2
ttzxh <63447869+ttzxh@users.noreply.github.com>
2020-04-20 21:46:08 +0800

    加载更多

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export function request(config){
 	const instance=axios.create({
-		baseURL:'http://152.136.185.210:8000/api/h8',
+		baseURL:'http://:8000/api/h8',
 		timeout:5000
 	})
 	
